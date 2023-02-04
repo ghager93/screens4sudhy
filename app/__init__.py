7e252608921e2e02d1eb2f0c3154f125a3eb9694
ghager93 <ghager93@gmail.com>
2023-02-04 21:23:13 +1100

    Moved main code to __init__

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+import logging
+
+from flask import Flask
+
+from app import core
+
+
+app = Flask(__name__)
+
+@app.route("/healthcheck", methods=["GET"])
+def healthcheck():
+    logging.info("healthcheck")
+    return 200
+
+
+@app.route("/screenshots", methods=["GET"])
+def get_screenshots():
+    try:
+        core.main_multithread()
+        return "Screenshots successfully created", 200
+    except:
+        return "An error occurred", 500     
+

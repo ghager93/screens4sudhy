953c32b82fd4b260aae9a99e6b7c24cfdba221d7
ghager93 <ghager93@gmail.com>
2023-02-04 21:44:46 +1100

    Download test

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
+import os
 import logging
 
-from flask import Flask
+from flask import Flask, send_file
 
 from app import core
 
@@ -9,8 +10,8 @@ app = Flask(__name__)
 
 @app.route("/healthcheck", methods=["GET"])
 def healthcheck():
-    logging.info("healthcheck")
-    return 200
+    logging.info("This is a healthcheck log.")
+    return "", 200
 
 
 @app.route("/screenshots", methods=["GET"])
@@ -19,5 +20,11 @@ def get_screenshots():
         core.main_multithread()
         return "Screenshots successfully created", 200
     except:
-        return "An error occurred", 500     
+        return "An error occurred", 500
+
+
+@app.route("/downloadtest", methods=["GET", "POST"])
+def download_test():
+    path = os.path.join(app.root_path, "core.py")
+    return send_file(path, as_attachment=True)
 
